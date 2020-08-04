666f059923687393100f73bc08bdd3cf81989fa8
Karamadi <basalingyya.channayya@siemens-healthineers.com>
2020-08-04 22:08:23 +0530

    dynamic reading data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { YamlDataService } from './yaml-data.service';
 import * as openApiModel from '../assets/jsondata.json';
 import {formModel} from './formmodel';
+import { YamlToJson } from './convertData';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,28 +11,16 @@ import {formModel} from './formmodel';
 export class AppComponent implements OnInit {
   public data;
   public formdata: Array<formModel>;
+  ymltoJson;
   constructor(private yamlDataService: YamlDataService) {}
   public ngOnInit() {
-    // this.data = this.yamlDataService
-    //   .fetchYaml(`testYaml.yaml`)
-    //   .subscribe((respose) => {
-    //     //console.log(respose);
-    //   });
-    this.data = (openApiModel as any).default;
-    this.data = this.data.components.schemas.Reward_Request;
-    let props = Object.entries(this.data.properties);
-    let reqFields = this.data.required;
-    console.log(Object.entries(this.data.properties));
-    props.forEach(item => {
-      let model:formModel;
-      console.log(item);
-      // model.propName = item[0];
-      // model.propType = item[1].type;
-      // model.propRequired = reqFields.indexOf(model.propName) !== -1 ? true : false;
-      //this.formdata.push(model);
-    });
-    console.log(this.formdata);
-  }
+    this.data = this.yamlDataService
+      .fetchYaml(`testYaml.yaml`)
+      .subscribe((respose) => {
+        const data = new YamlToJson().getYamlObject(respose);
+        console.log(data);
 
+      });
+  }
   title = 'open-api-poc';
 }
---
+++ src/app/convertData.ts
@@ -0,0 +1,9 @@
+import * as yaml from 'js-yaml';
+export class YamlToJson {
+  constructor() {}
+  getYamlObject(file) {
+    const doc = yaml.safeLoad(file, 'utf8');
+    console.log(doc);
+    return JSON.stringify(doc, null, 2);
+  }
+}
